6a626731669990aacaec278d4716638bc6b1c418
javieralarcn10 <alarconsanchezjavier@gmail.com>
2020-07-03 01:02:19 +0200

    proyecto terminado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const path = require('path')
 require('dotenv').config()
 const port = process.env.PORT;
@@ -8,7 +9,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({ extname: '.hbs', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', '.hbs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }))
 
+var cuenta = [];
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -20,6 +23,24 @@ app.get('/seleccion-menu', (req, res) => {
 
 app.get('/seleccion-complemento', (req, res) => {
     res.render('complemento')
+})
+
+app.get('/seleccion-postre', (req, res) => {
+    res.render('postre')
+})
+
+app.get('/seleccion-bebida', (req, res) => {
+    res.render('bebida')
+})
+
+app.get('/cuenta', (req, res) => {
+    res.render('cuenta', { cuenta: cuenta })
+    cuenta = [];
+})
+
+app.post('/save', (req, res) => {
+    cuenta.push(req.body)
+    res.send('ok')
 })
 
 
